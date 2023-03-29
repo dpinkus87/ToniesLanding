fccc09abb487ba0c833dc9a56ad4996091875521
dpinkus87 <dylanpinkus@gmail.com>
2023-03-28 20:36:47 -0500

    updates signup.js and home.handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,18 +2,19 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const user_name = document.querySelector('#username-signup').value.trim();
+  const first_name = document.querySelector('#firstname-signup').value.trim();
+  const last_name = document.querySelector('#lastname-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
   const address = document.querySelector('#address-signup').value.trim();
   const city = document.querySelector('#city-signup').value.trim();
   const zip = document.querySelector('#zip-signup').value.trim();
-  const bio = document.querySelector('#bio-signup').value.trim();
 
-  if (user_name && email && password && address && city && zip && bio && profile_image) {
+
+  if (first_name && && last_name && email && password && address && city && zip) {
     const response = await fetch('/api/users/signup', {
       method: 'POST',
-      body: JSON.stringify({ user_name, email, password, address, city, zip, bio, profile_image }),
+      body: JSON.stringify({ user_name, email, password, address, city, zip}),
       headers: { 'Content-Type': 'application/json' },
     });
 
